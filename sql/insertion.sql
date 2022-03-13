c20328ea1c3e16d08604c92536f2b3b1192a18ba
Mohand Kessi <mkessi@dept-info.crosemont.quebec>
2022-03-12 20:47:54 -0500

    scripts sql (création et insertion)
    Script de la création de la base de donnée
    Script de l'insertion des données

---
+++ sql/insertion.sql
@@ -0,0 +1,18 @@
+USE mediclic;
+
+-- Insérer des médecins
+INSERT INTO medecins (nom, prenom, specialite, numpro, coordonnes, facturation, sexe, mail, password)
+VALUES ("Marcel", "Justin", "Chirurgien", "(514)135-1264", "1892 Blvrd Côte-Vertu",  325, "Homme", "mjustin@gmail.com", "12345"),
+	   ("Floriance", "Sabine", "Chirurgien", "(514)526-2153", "145 Rue Crevier",  450, "Femme", "sfloriance@gmail.com", "crosemont"),
+	   ("River", "Ward", "Dentiste", "(514)532-7969", "192 Avenue 21",  120, "Homme", "rward@yahoo.ca", "lemotdepasse"),
+	   ("Alvarez", "Judy", "Generaliste", "(438)842-9931", "325 Kabuki",  250, "Femme", "ajudy@nightcity.com", "alvarez123"),
+	   ("Dupont", "Michel", "Octalmonogue", "(514)612-7691", "3262 Blvrd Poirier",  500, "Homme", "michel13@mediclic.qc.ca", "mediclic21");
+
+-- Insérer des patients
+INSERT INTO patients (nom, prenom, assurance, naissance, sexe, mail, password)
+VALUES ("Leblanc", "Richard", "LEBR2469274", "03-11-1973", "Homme", "brichard@gmail.com", "crosemont53"),
+	   ("Martine", "Sandra", "MARS2469743", "12-06-1990", "Femme", "Sany13@gmail.com", "rosemont12"),
+	   ("Decker", "Ella", "DECA2496704", "30-01-1986", "Femme", "Ella@hotmail.fr", "password64"),
+	   ("Carlier", "Serge", "CARS4367934", "13-09-1969", "Homme", "cserge@gmail.com", "cafelatte32"),
+	   ("Bouchard", "Cassandra", "BOUC3429670", "28-02-2001", "Femme", "cassandratec@crosemont.qc.ca", "billy43");
+
---
+++ sql/mediclic.sql
@@ -0,0 +1,37 @@
+-- Création de la base de donnée
+CREATE SCHEMA `mediclic` ;
+
+-- Ajout de la table patients
+CREATE TABLE `mediclic`.`patients` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nom` VARCHAR(45) NOT NULL,
+  `prenom` VARCHAR(45) NOT NULL,
+  `assurance` VARCHAR(45) NULL,
+  `naissance` VARCHAR(45) NULL,
+  `sexe` VARCHAR(45) NULL,
+  `mail` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`));
+
+-- Ajout de la table medecins
+CREATE TABLE `mediclic`.`medecins` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nom` VARCHAR(45) NULL,
+  `prenom` VARCHAR(45) NULL,
+  `specialite` VARCHAR(45) NULL,
+  `numpro` VARCHAR(45) NULL,
+  `coordonnes` VARCHAR(45) NULL,
+  `facturation` DOUBLE NULL,
+  `sexe` VARCHAR(45) NULL,
+  `mail` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`));
+
+-- Ajout de la table rdv
+CREATE TABLE `mediclic`.`rdv` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nompatient` VARCHAR(45) NULL,
+  `nommedecin` VARCHAR(45) NULL,
+  `date` VARCHAR(45) NULL,
+  `adresse` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`));
